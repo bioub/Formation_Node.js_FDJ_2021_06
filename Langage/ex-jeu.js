29218c828dbcb682b50a7a17321e6a850ea886f2
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-06-14 16:11:51 +0200

    Correction

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,21 +1,59 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // import l'API readline (présent dans le binaire de Node)
 const readline = require('readline');
+
+function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
+
+  rl.question('Quel est le nombre ? ', (answer) => {
+
+    const entierSaisi = parseInt(answer, 10);
+
+    if (isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
+  });
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-function jouer() {
-  rl.question('Quel est le nombre ? ', (answer) => {
-    console.log('Vous avez saisi : ' + answer);
-
-    // pour rejouer
-    // jouer();
-
-    // pour terminer la partie
-    rl.close();
-  });
-}
-
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
